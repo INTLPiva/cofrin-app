8ab9d0db073a8c52637919df08df4ae09dc7c64c
INTLPiva <lucas.piva@gec.inatel.br>
2026-02-12 12:09:28 -0300

    feat: List component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,6 @@
 import { HomeHeader } from "@/components/HomeHeader";
+import { List } from "@/components/List";
+import { Target } from "@/components/Target";
 import { View } from "react-native";
 
 const summary = {
@@ -7,10 +9,43 @@ const summary = {
   output: { label: "Saídas", value: "-R$ 883,65" },
 };
 
+const targets = [
+  {
+    id: "1",
+    name: "Apple Watch",
+    percentage: "50%",
+    current: "R$ 580,00",
+    target: "R$ 1.790,00",
+  },
+  {
+    id: "2",
+    name: "Comprar uma cadeira ergonômica",
+    percentage: "75%",
+    current: "R$ 900,00",
+    target: "R$ 1.200,00",
+  },
+  {
+    id: "3",
+    name: "Comprar uma viagem",
+    percentage: "75%",
+    current: "R$ 1.200,00",
+    target: "R$ 3.000,00",
+  },
+];
+
 export default function Index() {
   return (
     <View style={{ flex: 1 }}>
       <HomeHeader data={summary} />
+
+      <List
+        title="Metas"
+        data={targets}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Target data={item} />}
+        emptyMessage="Nenhuma meta. Toque em nova meta para criar."
+        containerStyle={{ paddingHorizontal: 24 }}
+      />
     </View>
   );
 }
