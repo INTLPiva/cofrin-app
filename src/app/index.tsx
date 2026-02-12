15f04f5870e0530f063d88de7a9fef6fe492fc73
INTLPiva <lucas.piva@gec.inatel.br>
2026-02-12 10:39:31 -0300

    feat: screens and default layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,27 @@
-import { Text, View } from "react-native";
+import { router } from "expo-router";
+import { Button, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Hello world!</Text>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 8,
+      }}
+    >
+      <Text>Index</Text>
+
+      <Button title="Nova meta" onPress={() => router.navigate("/target")} />
+      <Button
+        title="Transação"
+        onPress={() => router.navigate("/transaction/1")}
+      />
+      <Button
+        title="Progresso"
+        onPress={() => router.navigate("/in-progress/1")}
+      />
     </View>
   );
 }
---
+++ src/app/transaction/[id].tsx
@@ -0,0 +1,14 @@
+import { router, useLocalSearchParams } from "expo-router";
+import { Button, Text, View } from "react-native";
+
+export default function Transaction() {
+  const params = useLocalSearchParams<{ id: string }>();
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Transaction {params.id}</Text>
+
+      <Button title="Voltar" onPress={() => router.back()} />
+    </View>
+  );
+}
